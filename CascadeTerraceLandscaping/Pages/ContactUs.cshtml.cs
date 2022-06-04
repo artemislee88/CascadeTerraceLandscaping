0ac725f352e72ee6c8921e54bcd609fb80abb179
Artemis Lee <artemislee.88@outlook.com>
2022-06-04 03:59:35 -0400

    Create Mock Site

---
+++ CascadeTerraceLandscaping/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CascadeTerraceLandscaping.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
